81e692f00cbce6a0001e41dcf3ffd4d84fe9ead4
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-10-03 18:09:25 +0300

    completeHW

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-// import apiKey form "./javaScript/cat-api";
-// import catApi from "./javaScript/cat-api"
-// export { refs, addSelectBreed }
+
+import SlimSelect from 'slim-select'
+
+const selector = new SlimSelect({
+  select: "#selector",
+  showSearch: false,
+  settings: {
+    placeholderText: 'Pick breed for more information',
+  },
+})
+
+//
+//
+//
+// ======================================================================
+
 import { fetchBreeds, fetchBreedImg } from "./javaScript/cat-api.js"
+//
+// 
+// 
 // ======================================================================
 
 export const refs = {
@@ -11,65 +27,125 @@ export const refs = {
   breedsArray: [],
   card: document.querySelector('.cat-info'),
 }
-
+//
+//
+//
 // ======================================================================
+
 function populateBreedSelector() {
   fetchBreeds().then(
-  data => {
-    refs.breedsArray = data;
-    addSelectBreed();
+    data => {
+      refs.breedsArray = data;
+      // ================================
+      // addSelectBreed();
+
+      const nameArray = data.map(obj => (
+        { text: `${obj.name}`, value: `${obj.id}` }));
+      selector.setData(nameArray)
+      eventListener()
   }).catch(
     error => {
       console.error(error);
-      refs.errorMsg.classList.toggle("isInvisible");
+      errorToggle()
+      loadHide()
+      // refs.errorMsg.classList.toggle("isInvisible");
     }
 )
 }
 
 populateBreedSelector()
 
-export function addSelectBreed() {
-  refs.breedsArray.forEach(breedEl => {
-    const selectEl = document.createElement('option');
-    selectEl.value = breedEl.id;
-    selectEl.textContent = breedEl.name;
-    refs.breedSelector.appendChild(selectEl)
-  }
-  );
-  refs.breedSelector.classList.remove('isInvisible')
-};
-
+// function addSelectBreed() {
+//   refs.breedsArray.forEach(breedEl => {
+//     const selectEl = document.createElement('option');
+//     selectEl.value = breedEl.id;
+//     selectEl.textContent = breedEl.name;
+//     refs.breedSelector.appendChild(selectEl)
+//   }
+//   );
+//   refs.breedSelector.classList.remove('isInvisible')
+// };
+// 
+//
+//
 // =======================================================================
-refs.breedSelector.addEventListener('change', onSelectedBreed)
+
+function eventListener() {
+  refs.breedSelector.addEventListener('change', onSelectedBreed)
+  errorHide()
+}
 
 
 function onSelectedBreed(ev) {
-  refs.breedSelector.classList.add('isInvisible')
-  refs.loadingMsg.classList.remove("isInvisible")
-  
-// ======================================================
+  selectorDisabled()
+  // refs.loadingMsg.classList.remove("isInvisible")
+  loadShow()
+
+// ===============================================
   fetchBreedImg(ev.target.value).then(
     data => {
-    refs.loadingMsg.classList.add("isInvisible")
+      // refs.loadingMsg.classList.add("isInvisible")
+      loadHide()
+      // refs.card.classList.add("border")
+      borderShow()
+
+      // =========================================
+
     drawCard(data,ev.target.value)
     }).catch(
-    error => {console.log(error);
-    refs.errorMsg.classList.remove("isInvisible");
+      error => {
+        console.log(error);
+        errorToggle()
+        loadHide()
+    // refs.errorMsg.classList.remove("isInvisible");
   });
 };
 
 function drawCard(imgUrl, ev) {
-  refs.breedSelector.classList.remove('isInvisible')
+  // refs.breedSelector.classList.remove('isInvisible')
+  selectorActive()
   const cat = refs.breedsArray.find(element => element.id === ev);
   refs.card.innerHTML = `<img
+        class="img"
         src="${imgUrl[0].url}"
         alt="beautiful${cat.name}cat"
         width="480px"
+        height="560px"
       />
-      <h2>${cat.name}</h2>
-      <p>${cat.description}</p>
-      <p><span class="decorated">Temperament</span>${cat.temperament}</p>
-      <a href="${cat.wikipedia_url}" target="_blank" rel="noreferrer noopener">Wikipedia</a>`
+      <div class="text-wrapper">
+      <h2 class="headers">${cat.name}</h2>
+      <p class="text">${cat.description}</p>
+      <p class="text"><span class="decorated">Temperament: </span>${cat.temperament}</p>
+      <a class="headers wiki" href="${cat.wikipedia_url}" target="_blank" rel="noreferrer noopener" >Wikipedia</a>
+      </div>`     
 };
 
+function loadShow() {
+  refs.loadingMsg.classList.remove("isInvisible")
+}
 
+
+function loadHide() {
+  refs.loadingMsg.classList.add("isInvisible")
+}
+
+function errorToggle() {
+  refs.errorMsg.classList.toggle("isInvisible");
+}
+
+function errorHide() {
+  refs.errorMsg.classList.add("isInvisible");
+}
+
+
+function selectorDisabled() {
+  document.getElementById('selector').disabled = true;
+}
+
+function selectorActive() {
+  document.getElementById('selector').disabled = false;
+}
+
+function borderShow() {
+  refs.card.classList.add("border")
+}
